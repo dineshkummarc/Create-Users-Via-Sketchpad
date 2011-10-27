8244efc4cde6af58ca2feaa8fcaa801f159d0dd1
unknown <dwayne@dwayne.7digital.local>
2011-10-27 17:07:27 +0100

    first commit

---
+++ CreateUsers/CreateUsers.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+
+namespace CreateUsers
+{
+    internal class CreateUsers
+    {
+        private static readonly string ApiVersion = ConfigurationManager.AppSettings.Get(0);
+        private static readonly string ApiURL = ConfigurationManager.AppSettings.Get(1);
+        private static readonly string AccountURL = ConfigurationManager.AppSettings.Get(2);
+        private static readonly string OauthRequestTokenURL = ConfigurationManager.AppSettings.Get(3);
+        private static readonly string OauthAccesstokenURL = ConfigurationManager.AppSettings.Get(4);
+        private static readonly string UserAuthenticationURL = ConfigurationManager.AppSettings.Get(5);
+        private static readonly string SketchpadURL = ConfigurationManager.AppSettings.Get(6);
+        private static readonly string ConsumerKey = ConfigurationManager.AppSettings.Get(7);
+        private static readonly string ConsumerSecret = ConfigurationManager.AppSettings.Get(8);
+        private static readonly string Password = ConfigurationManager.AppSettings.Get(9);
+        private static readonly int Users = Convert.ToInt32(ConfigurationManager.AppSettings.Get(10));
+        private static readonly string UserEmailPrefix = ConfigurationManager.AppSettings.Get(11);
+        private static int _usersCreated;
+        private static int _usersFailed;
+        
+
+        public static void Main(string[] args)
+        {
+            
+            var oauthTokens = new string[Users];
+            var oauthTokenSecrets = new string[Users];
+
+            int oauthTokensIndex = 0;
+            int oauthTokenSecretsIndex = 0;
+
+            ResetUsersTotals();
+
+            for (int i = 0; i < Users; i++)
+            {
+                
+                var sketchpad = new Sketchpad();
+                sketchpad.Setup();
+
+                if (sketchpad.Driver != null)
+                {
+                    sketchpad.Driver.Navigate().GoToUrl(SketchpadURL);
+
+
+                    if (sketchpad.PopulateAndSignSketchpad(ApiURL + "/" + ApiVersion +"/"+ OauthRequestTokenURL,
+                                                           ConsumerKey,
+                                                           ConsumerSecret))
+                    {
+
+                        string tempOauthToken = sketchpad.ExtractOauthToken();
+                        string tempOauthTokenSecret = sketchpad.ExtractOauthTokenSecret();
+
+                        if (sketchpad.UserAuthentication(AccountURL + "/" + ConsumerKey +"/"+ UserAuthenticationURL +
+                                                         "?oauth_token=" +
+                                                         tempOauthToken +
+                                                         "&oauth_callback=http%3a%2f%2fapi-consumer.com%2fhandback%3fconsumerparam%3dvalue"))
+                        {
+                            sketchpad.Driver.Navigate().GoToUrl(SketchpadURL);
+                            if (sketchpad.PopulateAndSignSketchpad(ApiURL + "/" + ApiVersion + "/" + OauthAccesstokenURL,
+                                                                   ConsumerKey,
+                                                                   ConsumerSecret, tempOauthToken,
+                                                                   tempOauthTokenSecret))
+                            {
+
+                                string oauthToken = sketchpad.ExtractOauthToken();
+                                string oauthTokenSecret = sketchpad.ExtractOauthTokenSecret();
+
+                                if (oauthToken.EndsWith("==") && oauthTokenSecret.EndsWith("=="))
+                                {
+                                    _usersCreated = _usersCreated + 1;
+
+
+                                    oauthTokens[oauthTokensIndex] = oauthToken;
+                                    oauthTokensIndex = oauthTokensIndex + 1;
+                                    Console.WriteLine(oauthToken);
+                                    oauthTokenSecrets[oauthTokenSecretsIndex] = oauthTokenSecret;
+                                    oauthTokenSecretsIndex = oauthTokenSecretsIndex + 1;
+                                    Console.WriteLine(oauthTokenSecret);
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Firefox is not installed or has been closed by the user");
+                }
+                sketchpad.Teardown();
+            }
+            Console.WriteLine("Created " + _usersCreated + " users");
+            Console.WriteLine("Failed to create " + _usersFailed + " users");
+            WirteToCsv(oauthTokens, oauthTokenSecrets);
+        }
+
+        public static void UpdateUsersFailed()
+        {
+            _usersFailed = _usersFailed + 1;
+        }
+
+        private static void ResetUsersTotals()
+        {
+            _usersCreated = 0;
+            _usersFailed = 0;
+        }
+
+        public static string GetPassword()
+        {
+            return Password;
+        }
+
+        public static string GetUserEmailPrefix()
+        {
+            return UserEmailPrefix;
+        }
+
+        private static void WirteToCsv(string[] tokens, string[] tokensSecrets)
+        {
+            var theBuilder = new System.Text.StringBuilder();
+            
+            for (int i = 0; i < tokens.Length; i++)
+            {
+                theBuilder.Append(tokens[i]);
+                theBuilder.Append(",");
+                theBuilder.Append(tokensSecrets[i]);
+                theBuilder.Append("\n");
+            }
+
+            using (var theWriter = new StreamWriter("CreatedUsers.csv"))
+            {
+                theWriter.Write(theBuilder.ToString());
+            }
+        }
+    }
+}
---
+++ CreateUsers/Sketchpad.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System.Text.RegularExpressions;
+
+namespace CreateUsers
+{
+    public class Sketchpad
+    {
+        public IWebDriver Driver;
+
+        public void Setup()
+        {
+            Driver = new FirefoxDriver();
+        }
+        
+        public void Teardown()
+        {
+            Driver.Quit();
+        }
+        
+        public bool PopulateAndSignSketchpad(string url, string consumerKey, string consumerSecret)
+        {
+            try
+            {
+                IWebElement urlBox = Driver.FindElement(By.XPath(".//*[@id='apiurl']"));
+                if (urlBox != null) urlBox.SendKeys(url);
+
+                IWebElement consumerKeyBox = Driver.FindElement(By.XPath(".//*[@id='form1']/div/input[2]"));
+                if (consumerKeyBox != null) consumerKeyBox.SendKeys(consumerKey);
+
+                IWebElement consumerSecretBox = Driver.FindElement(By.XPath(".//*[@id='form1']/div/input[3]"));
+                if (consumerSecretBox != null) consumerSecretBox.SendKeys(consumerSecret);
+
+                IWebElement signRequestButton = Driver.FindElement(By.XPath(".//*[@id='form1']/div/input[6]"));
+                if (signRequestButton != null) signRequestButton.Click();
+
+                IWebElement openSignedURL = Driver.FindElement(By.XPath(".//*[@id='form1']/div/a[2]"));
+                if (openSignedURL != null) openSignedURL.Click();
+
+                return true;
+            }
+            catch (NoSuchElementException exception)
+            {
+                Console.WriteLine("Failed to create user: " + exception.Message);
+                CreateUsers.UpdateUsersFailed();
+                return false;
+            }
+        }
+        
+        public bool PopulateAndSignSketchpad(string url, string consumerKey, string consumerSecret, string tokenKey,
+                                             string tokenSecret)
+        {
+            try
+            {
+                IWebElement urlBox = Driver.FindElement(By.XPath(".//*[@id='apiurl']"));
+                if (urlBox != null) urlBox.SendKeys(url);
+
+                IWebElement consumerKeyBox = Driver.FindElement(By.XPath(".//*[@id='form1']/div/input[2]"));
+                if (consumerKeyBox != null) consumerKeyBox.SendKeys(consumerKey);
+
+                IWebElement consumerSecretBox = Driver.FindElement(By.XPath(".//*[@id='form1']/div/input[3]"));
+                if (consumerSecretBox != null) consumerSecretBox.SendKeys(consumerSecret);
+
+                IWebElement tokenKeyBox = Driver.FindElement(By.XPath(".//*[@id='form1']/div/input[4]"));
+                if (tokenKeyBox != null) tokenKeyBox.SendKeys(tokenKey);
+
+                IWebElement tokensecretBox = Driver.FindElement(By.XPath(".//*[@id='form1']/div/input[5]"));
+                if (tokensecretBox != null) tokensecretBox.SendKeys(tokenSecret);
+
+                IWebElement signRequestButton = Driver.FindElement(By.XPath(".//*[@id='form1']/div/input[6]"));
+                if (signRequestButton != null) signRequestButton.Click();
+
+                IWebElement openSignedURL = Driver.FindElement(By.XPath(".//*[@id='form1']/div/a[2]"));
+                if (openSignedURL != null) openSignedURL.Click();
+
+                return true;
+            }
+            catch (NoSuchElementException exception)
+            {
+                Console.WriteLine("Failed to create user: " + exception.Message);
+                CreateUsers.UpdateUsersFailed();
+                return false;
+            }
+        }
+        
+        public bool UserAuthentication(String url)
+        {
+            try
+            {
+                Driver.Navigate().GoToUrl(url);
+
+
+                IWebElement createEmail = Driver.FindElement(By.XPath(".//*[@id='SignUpData_Email']"));
+                if (createEmail != null)createEmail.SendKeys(CreateUsers.GetUserEmailPrefix() + Guid.NewGuid().ToString() + "@7digital.com");
+                   
+                IWebElement createPassword = Driver.FindElement(By.XPath(".//*[@id='SignUpData_Password']"));
+                if (createPassword != null) createPassword.SendKeys(CreateUsers.GetPassword());
+
+                IWebElement confirmPassword = Driver.FindElement(By.XPath(".//*[@id='SignUpData_PasswordConfirm']"));
+                if (confirmPassword != null) confirmPassword.SendKeys("test");
+
+                IWebElement agreeToTermsAndConditions =
+                    Driver.FindElement(By.XPath(".//*[@id='content']/form[2]/fieldset/label[4]/input"));
+                if (agreeToTermsAndConditions != null) agreeToTermsAndConditions.Click();
+
+                IWebElement signMeUpButton = Driver.FindElement(By.XPath(".//*[@id='SignUp.Submit']"));
+                if (signMeUpButton != null) signMeUpButton.Click();
+
+                IWebElement allowButton =
+                    Driver.FindElement(By.XPath(".//*[@id='content']/ul/li[1]/form/fieldset/button"));
+                if (allowButton != null) allowButton.Click();
+                return true;
+            }
+            catch (NoSuchElementException exception)
+            {
+                Console.WriteLine("Failed to create user: " + exception.Message);
+                CreateUsers.UpdateUsersFailed();
+                return false;
+            }
+        }
+
+        public String ExtractOauthToken()
+        {
+            try
+            {
+                const string oauthTokenPattern = "<oauth_token>(.*?)</oauth_token>";
+                var oauthTokenRegex = new Regex(oauthTokenPattern);
+                string oauthToken = oauthTokenRegex.Match(Driver.PageSource).Groups[1].Value;
+
+                return oauthToken;
+            }
+            catch (NoSuchElementException exception)
+            {
+                Console.WriteLine("Failed to create user: " + exception.Message);
+                CreateUsers.UpdateUsersFailed();
+                return null;
+            }
+        }
+
+        public String ExtractOauthTokenSecret()
+        {
+            try
+            {
+
+                const string oauthTokenSecretPattern = "<oauth_token_secret>(.*?)</oauth_token_secret>";
+                var oauthTokenSecretRegex = new Regex(oauthTokenSecretPattern);
+                string oauthTokenSecret = oauthTokenSecretRegex.Match(Driver.PageSource).Groups[1].Value;
+
+                return oauthTokenSecret;
+            }
+            catch (NoSuchElementException exception)
+            {
+                Console.WriteLine("Failed to create user: " + exception.Message);
+                CreateUsers.UpdateUsersFailed();
+                return null;
+            }
+        }
+
+        public void PopulateAndSignSketchpad ()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
